5de133d6bb6074e6856e59587b085706fcd56026
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-10-08 15:57:01 +0300

    Fourth Commit - FirtstHomeTask
    Validation for negative values added.
    Validation for not Double values in progress.

--- src/test/java/FirstHomeTask.java
+++ src/test/java/FirstHomeTask.java
@@ -2,8 +2,11 @@ import org.apache.commons.math3.util.Precision;  // to use math functions
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.Scanner;             //to read inputs from console using Java Scanner
+import java.util.InputMismatchException;
 
 public class FirstHomeTask {
+
+    static Scanner valueReader = new Scanner(System.in);    // defining the Scanner
 
     @Test
     public static void main(String[] args) {                //static String[] args needs to read the values from console
@@ -11,24 +14,54 @@ public class FirstHomeTask {
         Double loanWithBankRates;
         Double stupidClerkCheck;
 
-        Scanner valueReader = new Scanner(System.in);       // defining the Scanner
+       // System.out.print("Insert Clients Amount: ");
+        isDouble();
+        loan = negativeValueCheck(valueReader.nextDouble());                     // reading the loan value using the scanner and validating it;
 
-        System.out.print("Insert Clients Amount: ");
-        loan = valueReader.nextDouble();                    // reading the loan value using the scanner;
         System.out.print("Clerk check: ");
-        stupidClerkCheck = valueReader.nextDouble();        // reading the clerks calculated value with bank rates;
+        stupidClerkCheck = negativeValueCheck(valueReader.nextDouble());        // reading the clerks calculated value with bank rates and validating it;
 
         loanWithBankRates = amountWithBankRatesCalculator(loan);
 
         Assertions.assertEquals(stupidClerkCheck, loanWithBankRates, "You dummy clerk! Get back to school!");  //check if the clerk is compliant to work in our bank
     }
 
-    private static Double amountWithBankRatesCalculator(Double a){
+    private static Double amountWithBankRatesCalculator(Double loan){
         Double firstTenYearsRate = 1.1;
         Double secondTenYearsARate = 1.08;
         Double thirdTenYearsRate = 1.06;
-        Double result = Precision.round(a/3*firstTenYearsRate + a/3*secondTenYearsARate + a/3*thirdTenYearsRate,2);    // round of the total sum client needs to pay for 2 decimals
+        Double result = Precision.round(loan/3*firstTenYearsRate + loan/3*secondTenYearsARate + loan/3*thirdTenYearsRate,2);    // round of the total sum client needs to pay for 2 decimals
         System.out.println("Amount to pay with bank rates after 30 years: " + result);     // just to now how much moneys the Client needs to pay
         return result;
+    }
+
+    private static Double negativeValueCheck (Double testValue){
+        do {
+            System.out.print("Clients Amount can't be negative! Try again: ");
+            testValue = valueReader.nextDouble();
+        } while (testValue < 0);
+        return testValue;
+    }
+
+    private static Double isDouble (){   // not working
+        Double testValue;
+        boolean checker = true;
+        
+        do {
+            try {
+                System.out.print("Insert Clients Amount: ");
+                testValue = valueReader.nextDouble();
+                checker = false;
+            }
+            catch (InputMismatchException e) {
+                System.out.println(testValue + " is not a number");
+               // checker = false;
+                valueReader.next();
+            }
+//            finally {
+//                    System.out.println(testValue + " is not a number");
+//            }
+        } while (checker);
+        return testValue;
     }
 }
