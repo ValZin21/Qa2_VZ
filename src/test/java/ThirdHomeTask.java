afd9c361c5eb8f83deab5841f6de5b0b978fe2fd
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-11-01 15:01:41 +0200

    ThirdHomeTask
    First part ready
    Timer established, page switching and filtering works, check if iterations are correct added.

--- src/test/java/ThirdHomeTask.java
+++ src/test/java/ThirdHomeTask.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,13 +14,12 @@ public class ThirdHomeTask {
     private final By WOMAN_BUTTON_SEARCH = By.xpath(".//li/a[@title='Women']");
     private final By DRESSES_BUTTON_SEARCH = By.xpath(".//ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a[@title='Dresses']");
     private final By ORANGE_FILTER_SEARCH = By.xpath(".//*[@id='layered_id_attribute_group_13']");
-    private final By aaa = By.xpath(".//*[text()='Dresses > Color Orange']");
-    private final By bbb = By.xpath(".//*[@class='page-heading product-listing']/text()='Dresses > Color Orange'");
-    private final By ccc = By.xpath(".//h1/span[@class='cat-name']/text()='Dresses > Color Orange'");
-    private final By ddd = By.xpath(".//h1/span[contains(text(), 'Dresses > Color Orange')]");
-    private final By eee = By.xpath(".//h1/span[@class='cat-name']");
-    //private final By aaa = By.xpath(".//h1/span[@class='cat-name']");
-   // private final By aaa = By.xpath(".//h1/span[@class='cat-name']");
+//    private final By aaa = By.xpath(".//*[text()='Dresses > Color Orange']");
+//    private final By bbb = By.xpath(".//*[@class='page-heading product-listing']/text()='Dresses > Color Orange'");
+//    private final By ccc = By.xpath(".//h1/span[@class='cat-name']/text()='Dresses > Color Orange'");
+    private final By FILTER_CHECK = By.xpath(".//h1/span[contains(text(), 'Dresses > Color Orange')]");
+//    private final By eee = By.xpath(".//h1/span[@class='cat-name']");
+
     public WebDriver driver;
 
     @Test
@@ -45,38 +42,19 @@ public class ThirdHomeTask {
 
         String dressesPageTitle = driver.getTitle();
         Assertions.assertFalse(dressesPageTitle == womenPageTitle, "Page not changed from WOMEN to DRESSES");
-        System.out.println("Page title before: "  + driver.getTitle());
+        System.out.println("Page title before filter is applied: "  + driver.getTitle());
         driver.findElement(ORANGE_FILTER_SEARCH).click();
+        //timer to wait for filter is applied
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);  // try-catch wuld be nice to receive a beautiful message in case element is not found
+        //element which appears after filtering only - until it not detected code execution won't go forward
+        WebElement elementAppearedAfterFitlteringCheck = driver.findElement(FILTER_CHECK);
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement test = driver.findElement(ddd);
-       // Thread.sleep(10000);
-       // WebDriverWait waiter = new WebDriverWait(driver, 10);
-
-       // driver.navigate().to("http://automationpractice.com/index.php?id_category=8&controller=category#/color-orange");
-       // driver.get("http://automationpractice.com/index.php?id_category=8&controller=category#/color-orange");
-       // driver.navigate().to("http://automationpractice.com/index.php?id_category=8&controller=category#/color-orange");
-        //driver.navigate().refresh();
-
-        System.out.println("Title: " + driver.getTitle());
-
-        String karl = driver.findElement(ddd).getText();
-        System.out.println("This is filter: " + karl);
-//       // System.out.println("Page source: "  + driver.getPageSource());
-//        System.out.println("Page title: "  + driver.getTitle().c);
-//        System.out.println("Page url: "  + driver.getCurrentUrl());
-
-     //   driver.findElement(aaa).e
+        System.out.println("Page title after filter is applied: " + driver.getTitle());
+        String karl = driver.findElement(FILTER_CHECK).getText();
+        System.out.println("This is filtered product text: " + karl);
 
         //add check if Orange filter present
-
-       // This xpath .//text()='Dresses > Color Orange' return 1. Think how to check 'if 1 then succees, if 0 - error'
-
-      //  Assertions.assertTrue(driver.getPageSource().contains("Dresses > Color Orange"));
-     //   Assertions.assertEquals("Dresses > Color Orange", driver.findElement(ddd).getText(), "Orange filter is not applied!");
-     //   Assertions.assertNotEquals(orangeFilterCheck, driver.getCurrentUrl(), "PIPEC!");
-
-
+        Assertions.assertEquals("DRESSES > COLOR ORANGE", driver.findElement(FILTER_CHECK).getText(), "Orange filter is not applied!");
     }
 
     @AfterEach
