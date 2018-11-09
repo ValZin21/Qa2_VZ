7368b7eefd13578ad84a59f67f84bbc5fe9e4174
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-11-09 12:23:58 +0200

    Third home task
    Code ready. Works stable.
    Refactoring needed.

--- src/test/java/ThirdHomeTask.java
+++ src/test/java/ThirdHomeTask.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -27,7 +28,6 @@ public class ThirdHomeTask {
     private final By FILTERED_PRODUCTS_COLOR_LISTS = By.xpath(".//*[@class='color-list-container']");
     private final By PRODUCT_ORANGE_COLOR = By.xpath(".//*[@class='color-list-container']/ul/li/a[@style='background:#F39C11;']");
     private final By FILTERED_PRODUCTS_COLOR_LISTS_COUNT = By.xpath(".//*[@class='heading-counter']/span");
-
 
     private final By PRODUCT_CLICK = By.xpath(".//*[@class = 'product_img_link']");
     private final By IF_PRODUCT_IS_ORANGE = By.xpath(".//a[@name='Orange']");
@@ -51,8 +51,9 @@ public class ThirdHomeTask {
     private static final Logger LOGGER = LogManager.getLogger(ThirdHomeTask.class);
 
     public WebDriver driver;
-    List<WebElement> filteredProducts;
-    List<WebElement> inStockProducts;
+    List<WebElement> filteredProducts = new ArrayList<WebElement>();
+    List<WebElement> inStockProducts = new ArrayList<WebElement>();
+    List<String> filteredProductsPrices = new ArrayList<String>();
 
     @Test
     public void ladiesStore() {
@@ -94,7 +95,7 @@ public class ThirdHomeTask {
         //add check if Orange filter present
         Assertions.assertEquals("DRESSES > COLOR ORANGE", driver.findElement(FILTER_CHECK).getText(), "Orange filter is not applied!");
 
-        //Check if filtered elements has orange color and it count is the same as mareked count - point 4 (REFACTOR REQUIRED) here
+        //point 4 - Check if filtered elements has orange color and it count is the same as mareked count(REFACTOR REQUIRED) here
         filteredProducts = driver.findElements(FILTERED_PRODUCTS_COLOR_LISTS);
         Assertions.assertTrue(!filteredProducts.isEmpty(), "No filtered products detected!");
         LOGGER.info(filteredProducts.size());
@@ -135,7 +136,9 @@ public class ThirdHomeTask {
         Assertions.assertTrue(!filteredProducts.isEmpty(), "Add To cart butonn missed!");
 //        driver.manage().timeouts().implicitlyWait(14, TimeUnit.SECONDS);
         double totalRevenue = 0.0;
+        //Adding filtered products to cart. Collecting product prices to filteredProductsPrices list
         Actions manipulator = new Actions(driver);
+
         for (int i = 0; i < filteredProducts.size(); i++) {
 
             LOGGER.info(filteredProducts.size());
@@ -147,12 +150,13 @@ public class ThirdHomeTask {
 
             LOGGER.info("StringPrice: " + filteredProducts.get(i).findElement(PRODUCT_PRICE).getText());
 //            totalRevenue = totalRevenue + Double.valueOf(filteredProducts.get(i).findElement(PRODUCT_PRICE).getText().substring(1));
+            //not needed
             totalRevenue = productsTotalRevenue(i, totalRevenue);
             LOGGER.info("totalRevenues: " + totalRevenue);
 
             filteredProducts.get(i).findElement(ADD_TO_CART_CLICK).click();
 //            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-            driver.switchTo().activeElement();
+//            driver.switchTo().activeElement();
             wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(CONTINUE_SHOPPING_CLICK)));
             LOGGER.info("It's time to kick ass and chew buuble gum!");
             driver.findElement(CONTINUE_SHOPPING_CLICK).click();
@@ -160,44 +164,55 @@ public class ThirdHomeTask {
 
 //            wait.until(ExpectedConditions.elementToBeClickable(filteredProducts.get(i).findElement(PRODUCT_PRICE)));
 
-
-
-            driver.switchTo().defaultContent();
+//            driver.switchTo().defaultContent();
         }
 
+        //not needed
         totalRevenue = roundPriceToTwoSymbols(totalRevenue);
         LOGGER.info("Total revenue after convertation: " + totalRevenue);
 
+        //opening Shopping Carts Drop-down menu
 //        new Actions(driver).moveToElement(driver.findElement(SHOPPING_CART)).perform();
         manipulator.moveToElement(driver.findElement(SHOPPING_CART)).perform();
+       // driver.switchTo().activeElement();
+        wait.until(ExpectedConditions.elementToBeClickable(PRODUCT_IN_CART));
+
+        Double totalRevenue2 = 0.0;
 
         inStockProducts = driver.findElements(PRODUCT_IN_CART);
         for (int i = 0; i < inStockProducts.size(); i++) {
             Assertions.assertEquals(
-                    filteredProducts.get(i).findElement(PRODUCT_PRICE).getText(),
+                    //comparing if added elements prices match Etalons
+                    filteredProductsPrices.get(i),
                     inStockProducts.get(i).findElement(PRODUCT_IN_CART_PRICE).getText(),
                     "Product " + (i+1) + " prices mismatch!");
+
+            LOGGER.info("Product " + (i+1) + " price validation succeed!");
+
+            totalRevenue2 = productsTotalRevenue2(i, totalRevenue2);
+
+            if ((inStockProducts.size() - i) == 1 ) {
+                //checking if total price calculated correctly (s)
+                totalRevenue2 = roundPriceToTwoSymbols(totalRevenue2);
+                totalRevenue2 = totalRevenue2 + Float.valueOf(driver.findElement(PRODUCTS_SHIPPING_PRICE).getText().substring(1));
+                Assertions.assertEquals(totalRevenue2, Double.valueOf(driver.findElement(PRODUCTS_TOTAL_PRICE).getText().substring(1)), "Totals mismatch!");
+            }
         }
 
-        LOGGER.info("Product price validation succeed!");
-
-
+        LOGGER.info("You made it!");
 
         /*to do
-        ** find products in cart
-        ** check that correct products are added in cart (in cycle names = in cart names)
-        ** compare prices??? probably - not needed
-        ** compare the in cycle total revenue with in cart total revenue (in cart total revenue - shipping price)
+        ** refactor the code!
         */
     }
 
 
 
-//    @AfterEach
-//    public void driverClose(){
-//        driver.close();
-//        driver.quit();
-//    }
+    @AfterEach
+    public void driverClose(){
+        driver.close();
+        driver.quit();
+    }
 
     public int filteredBracketsNumberCheck(){
         String text = driver.findElement(ORANGE_FILTER_PRODUCT_COUNT).getText();
@@ -213,8 +228,14 @@ public class ThirdHomeTask {
         return result;
     }
 
-    public double productsTotalRevenue (int i, double totalPrice) {
+    public double productsTotalRevenue (int i, double totalPrice) { //this method not needed
         totalPrice = totalPrice + Double.valueOf(filteredProducts.get(i).findElement(PRODUCT_PRICE).getText().substring(1));
+        filteredProductsPrices.add(filteredProducts.get(i).findElement(PRODUCT_PRICE).getText());
+        return totalPrice;
+    }
+
+    public double productsTotalRevenue2 (int i, double totalPrice) {
+        totalPrice = totalPrice + Double.valueOf(inStockProducts.get(i).findElement(PRODUCT_IN_CART_PRICE).getText().substring(1));
         return totalPrice;
     }
 
