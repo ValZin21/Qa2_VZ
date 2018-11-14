e1bcd90411d7588add8aa07a50c9d37f9aa8b7e8
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-11-14 13:14:44 +0200

    Lesson 6 Class Work
    Code refactored to avoid scrolling to comments count bellow article text.
    Solution: detect article comments count from attribute of the element placed in the beginning of the page.

--- src/test/java/TvNetTest.java
+++ src/test/java/TvNetTest.java
@@ -24,15 +24,13 @@ public class TvNetTest {
     private final By ARTICLE_TITLE = By.xpath(".//*[@class='list-article__headline']");
     private final By COMMENT_COUNT = By.xpath(".//span[contains(@class, 'list-article__comment')]");
     private final By ARTICLE_AND_COMMENTS_PAGES_TITLE = By.xpath(".//*[contains(@class, 'headline')]");
-    private final By ARTICLE_PAGE_COMENT_COUNT = By.xpath(".//div/span[contains(@class, 'article-comments-heading__count')]");
-    private final By COMMENTS_BTN = By.xpath(".//a[contains(@class, 'button--comment')]");
+    private final By COMMENTS_PAGE_COMENT_COUNT = By.xpath(".//div/span[contains(@class, 'article-comments-heading__count')]");
+    private final By ARTICLE_PAGE_COMENT_COUNT = By.xpath(".//a[contains(@class, 'article-share__item--comments')]");
     private final By LOWER_ADVERTISING = By.xpath(".//*[contains(@id, 'google_ads_iframe_/84367975')]/div[contains(@style, 'position')]");
     private final By LOWER_ADV_CLOSE_BUTTON = By.xpath(".//*[contains(@id, 'google_ads_iframe_/84367975')]/div[contains(@style, 'position')]");
-    private final By COMMENTS_SECTION = By.xpath(".//*[@class = 'article-terms']");
 
     private WebDriver browser;
     private WebDriverWait wait;
-    private Actions manipulator;
     private static final Logger LOGGER = LogManager.getLogger(TvNetTest.class);
 
     private List<WebElement> articles;
@@ -42,7 +40,6 @@ public class TvNetTest {
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
         browser = new ChromeDriver();
         wait = new WebDriverWait(browser, 15);
-        manipulator = new Actions(browser);
 
         browser.manage().window().maximize();
         browser.get(HOME_PAGE);
@@ -60,33 +57,30 @@ public class TvNetTest {
         testArticle.click();
 
 
+        isElementPresent(ARTICLE_AND_COMMENTS_PAGES_TITLE);
         String articlePageTitle = textGet(ARTICLE_AND_COMMENTS_PAGES_TITLE, null);
         LOGGER.info("Article page title: " + articlePageTitle);
         Assertions.assertTrue(articlePageTitle.contains(articleTitle), "Article page is wrong!");
         LOGGER.info("Article page title check pass");
 
-        scroolToCommetns();
-
         closeTheBanner();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(ARTICLE_PAGE_COMENT_COUNT));
-
-        Integer articlePageCommentCount = getCommentsNumber(textGet(ARTICLE_PAGE_COMENT_COUNT, null));
+        Integer articlePageCommentCount = getArticlePageCommentsCount();
         LOGGER.info("Article page comments count: " + articlePageCommentCount);
         Assertions.assertTrue(commentCount == articlePageCommentCount, "Wrong Article page Comment Count");
         LOGGER.info("Atricle page comments count check pass");
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(COMMENTS_BTN));
-        browser.findElement(COMMENTS_BTN).click();
+        articlePageClick();
 
 
+        isElementPresent(ARTICLE_AND_COMMENTS_PAGES_TITLE);
         String commentsPageTitle = textGet(ARTICLE_AND_COMMENTS_PAGES_TITLE, null);
         Assertions.assertTrue(commentsPageTitle.contains(articlePageTitle), "Comments page is wrong!");
         LOGGER.info("Comments page title check pass");
 
-        Integer commentPageCommentCount = getCommentsNumber(textGet(ARTICLE_PAGE_COMENT_COUNT, null));
+        Integer commentPageCommentCount = getCommentsNumber(textGet(COMMENTS_PAGE_COMENT_COUNT, null));
         LOGGER.info("Comments page comment count: " + commentPageCommentCount);
-        Assertions.assertTrue(commentCount == commentPageCommentCount, "Wrong Article page Comment Count");
+        Assertions.assertTrue(commentCount == commentPageCommentCount, "Wrong Comment page Comment Count");
         LOGGER.info("Comments page comments count check pass");
     }
 
@@ -108,14 +102,22 @@ public class TvNetTest {
         return Integer.valueOf(text);
     }
 
-    private void scroolToCommetns() {
-        manipulator.moveToElement(browser.findElement(COMMENTS_SECTION)).perform();
-        LOGGER.info("Page scrolled to comment count");
-    }
-
     private void closeTheBanner() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(LOWER_ADVERTISING));
+        isElementPresent(LOWER_ADVERTISING);
         browser.findElement(LOWER_ADV_CLOSE_BUTTON).click();
         LOGGER.info("Lower advertising closed");
+    }
+
+    private Integer getArticlePageCommentsCount() {
+        return Integer.valueOf(browser.findElement(ARTICLE_PAGE_COMENT_COUNT).getAttribute("data-content"));
+    }
+
+    private void isElementPresent(By xPath) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(xPath));
+    }
+
+    private void articlePageClick() {
+        wait.until(ExpectedConditions.elementToBeClickable(ARTICLE_PAGE_COMENT_COUNT));
+        browser.findElement(ARTICLE_PAGE_COMENT_COUNT).click();
     }
 }
