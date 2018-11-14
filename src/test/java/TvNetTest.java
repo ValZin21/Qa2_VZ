c31708787261dfecba10a5c748bcd824f08073b9
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-11-14 13:47:39 +0200

    Lesson 6 Class Work
    Leak Libraries Removed.

--- src/test/java/TvNetTest.java
+++ src/test/java/TvNetTest.java
@@ -4,19 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class TvNetTest {
     private final String HOME_PAGE = "https://www.tvnet.lv/";
