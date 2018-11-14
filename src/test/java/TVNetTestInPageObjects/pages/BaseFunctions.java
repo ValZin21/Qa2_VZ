f4e6741f41c5cbc23a3a59efb506280a8a7ce738
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-11-14 14:12:34 +0200

    Lesson 6 Class Work in PageObjects
    BaseFunctions creation in progress.

---
+++ src/test/java/TVNetTestInPageObjects/pages/BaseFunctions.java
@@ -0,0 +1,27 @@
+package TVNetTestInPageObjects.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseFunctions {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public BaseFunctions() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 15);
+        driver.manage().window().maximize();
+    }
+
+    public void goToPage(String url) {
+        if (!url.contains("http://") && !url.contains("https://")) {
+            url = "http://" + url;
+        }
+        driver.get(url);
+    }
+
+    //add more functional here
+}
