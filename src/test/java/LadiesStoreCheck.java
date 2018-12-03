928412a0fe06e4dd316dc554baf6a8dd5bf61e8f
valerijs.zincenko <valerijs.zincenko@accenture.com>
2018-12-03 11:19:50 +0200

    Third Home Work
    ImplicityWailt removed.
    New method added.
    Some xpath refactored.

--- src/test/java/LadiesStoreCheck.java
+++ src/test/java/LadiesStoreCheck.java
@@ -13,34 +13,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class LadiesStoreCheck {
 
     private final String HOME_PAGE = "http://automationpractice.com/index.php";
     private final By WOMAN_BUTTON_SEARCH = By.xpath(".//li/a[@title='Women']");
     private final By DRESSES_BUTTON_SEARCH = By.xpath(".//ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a[@title='Dresses']");
-    private final By ORANGE_FILTER_SEARCH = By.xpath(".//*[@id='layered_id_attribute_group_13']");
+    private final By ORANGE_FILTER_SEARCH = By.id("layered_id_attribute_group_13");
     private final By ORANGE_FILTER_PRODUCT_COUNT = By.xpath(".//*[@class='layered_color']/a[text()='Orange']/span");
     private final By FILTER_CHECK = By.xpath(".//h1/span[contains(text(), 'Dresses > Color Orange')]");
 
-    private final By FILTERED_PRODUCTS_COLOR_LISTS = By.xpath(".//*[@class='color-list-container']");
+    private final By FILTERED_PRODUCTS_COLOR_LISTS = By.className("color-list-container");
     private final By FILTERED_PRODUCTS_COLOR_LISTS_COUNT = By.xpath(".//*[@class='heading-counter']/span");
 
-    private final By PRODUCT_CLICK = By.xpath(".//*[@class = 'product_img_link']");
+    private final By PRODUCT_CLICK = By.className("product_img_link");
     private final By IF_PRODUCT_IS_ORANGE = By.xpath(".//a[@name='Orange']");
     private final By CLOSE_PRODUCT = By.xpath(".//*[@title = 'Close']");
 
-//    private final By ADD_TO_CART_CLICK = By.xpath(".//a[contains(@class, 'ajax_add_to_cart_button')]");
     private final By ADD_TO_CART_CLICK = By.xpath(".//a[@title = 'Add to cart']/span");
     private final By CONTINUE_SHOPPING_CLICK = By.xpath(".//span[contains(@title, 'Continue shopping')]");
-//    private final By SELECT_PRODUCT = By.xpath(".//*[@class='product-container']");
     private final By SELECT_PRODUCT = By.xpath(".//div[contains(@class, 'product-container')]");
     private final By PRODUCT_PRICE = By.xpath(".//*[@itemprop='price']");
 
     private final By SHOPPING_CART = By.xpath(".//*[@title='View my shopping cart']");
     private final By PRODUCT_IN_CART = By.xpath(".//*[contains(@data-id, 'cart_block_product')]");
-    private final By PRODUCT_IN_CART_PRICE = By.xpath(".//*[@class='price']");
+    private final By PRODUCT_IN_CART_PRICE = By.className("price");
     private final By PRODUCTS_SHIPPING_PRICE = By.xpath(".//*[contains(@class, 'cart_block_shipping_cost')]");
     private final By PRODUCTS_TOTAL_PRICE = By.xpath(".//*[contains(@class, 'cart_block_total')]");
 
@@ -79,8 +76,6 @@ public class LadiesStoreCheck {
         LOGGER.info("Page title before filter is applied: "  + driver.getTitle());
 
         elementClick(ORANGE_FILTER_SEARCH, 0, null);
-
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         wait.until(ExpectedConditions.presenceOfElementLocated(FILTER_CHECK));
         String elementAppearedAfterFitlteringCheck = textGet(FILTER_CHECK, 0, null);
         LOGGER.info("Page title after filter is applied: " + driver.getTitle());
@@ -90,15 +85,14 @@ public class LadiesStoreCheck {
 
         //point 4 - Check if filtered elements has orange color and it count is the same as mareked count(REFACTOR REQUIRED) here
         filteredProducts = getElements(FILTERED_PRODUCTS_COLOR_LISTS);
-        Assertions.assertTrue(!filteredProducts.isEmpty(), "No filtered products detected!");
-        LOGGER.info(filteredProducts.size());
+        Assertions.assertFalse(filteredProducts.isEmpty(), "No filtered products detected!");
         Assertions.assertEquals(filteredProducts.size(), filteredBracketsNumberCheck(),"Filter in-breakets product count mismatch with filtered products count");
         Assertions.assertEquals(filteredProducts.size(), filteredProductCountNumber(),"Filtered product count mismatch with total filtered product count text");
 
         //point5 - open 1 random item and check if orange is selected
         filteredProducts.clear();
         filteredProducts = getElements(PRODUCT_CLICK);
-        Assertions.assertTrue(!filteredProducts.isEmpty(), "No filtered products to click on detected!");
+        Assertions.assertFalse(filteredProducts.isEmpty(), "No filtered products to click on detected!");
 
         int randomProductSelector = 0 + (int) (Math.random() * filteredProducts.size());
         LOGGER.info("Random is: " + randomProductSelector);
@@ -114,13 +108,11 @@ public class LadiesStoreCheck {
 
         //POINT 6
         filteredProducts = getElements(SELECT_PRODUCT);
-        Assertions.assertTrue(!filteredProducts.isEmpty(), "Add To cart butonn missed!");
+        Assertions.assertFalse(filteredProducts.isEmpty(), "Add To cart butonn missed!");
         
         Actions moveCursor = new Actions(driver);
 
         for (int i = 0; i < filteredProducts.size(); i++) {
-
-            LOGGER.info(filteredProducts.size());
             moveCursor.moveToElement(filteredProducts.get(i)).perform();
             wait.until(ExpectedConditions.elementToBeClickable(getElement(ADD_TO_CART_CLICK, i, filteredProducts)));
             LOGGER.info("StringPrice: " + textGet(PRODUCT_PRICE , i, filteredProducts));
@@ -163,10 +155,10 @@ public class LadiesStoreCheck {
         LOGGER.info("FIN.");
     }
 
-//    @AfterEach
-//    private void driverClose(){
-//        driver.quit();
-//    }
+    @AfterEach
+    private void driverClose(){
+        driver.quit();
+    }
 
     private int filteredBracketsNumberCheck(){
         String text = textGet(ORANGE_FILTER_PRODUCT_COUNT, 0, null);
