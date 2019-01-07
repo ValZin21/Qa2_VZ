203fcd97901d273fbc6d22ee173de609b66b3e27
valerijs.zincenko <valerijs.zincenko@accenture.com>
2019-01-07 16:29:21 +0200

    Lesson 11 Class Work
    Models for WeatherRequester implemented.
    JsonIgnoreProperties tg commented for Response class as all response JSON parameters are covered now.

--- src/test/java/apiSteps/model/Response.java
+++ src/test/java/apiSteps/model/Response.java
@@ -5,12 +5,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true) // ignore all properties (which were not defined in JSON model but received from server)
+//@JsonIgnoreProperties(ignoreUnknown = true)
+// ignore all properties (which were not defined in JSON model but received from server)
 public class Response {
     private Coord coord;
     @JsonProperty("weather") // weathers will be weather in JSON
     private List<Weather> weathers;
     private String base;
+    private Main main;
+    private int visibility;
+    private Wind wind;
+    private Clouds clouds;
+    private String dt;
+    private Sys sys;
+    private String id;
+    private String name;
+    private String cod;
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
 
     public Coord getCoord() {
         return coord;
@@ -34,5 +48,73 @@ public class Response {
 
     public void setBase(String base) {
         this.base = base;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(Clouds clouds) {
+        this.clouds = clouds;
+    }
+
+    public String getDt() {
+        return dt;
+    }
+
+    public void setDt(String dt) {
+        this.dt = dt;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+    public void setSys(Sys sys) {
+        this.sys = sys;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
     }
 }
---
+++ src/test/java/apiSteps/model/Sys.java
@@ -0,0 +1,58 @@
+package apiSteps.model;
+
+public class Sys {
+    private int type;
+    private String id;
+    private double message;
+    private String country;
+    private String sunrise;
+    private String sunset;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getMessage() {
+        return message;
+    }
+
+    public void setMessage(double message) {
+        this.message = message;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+}
